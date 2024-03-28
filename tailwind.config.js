99f5f76ed7de7fe25d23445182bea4ab7aae6a6c
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-28 16:49:31 -0400

    Update with letter spacing and screen sizes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      letterSpacing: {
+        widest: '.3em',
       },
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
   },
   plugins: [],
-}
+};
