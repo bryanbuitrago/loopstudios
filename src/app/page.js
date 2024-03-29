455d94e1d9bedae25edaa8f848eda132783ad181
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-29 00:34:46 -0400

    Add FeatureSection component.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import FeatureSection from '@/components/features/FeatureSection';
 import HeroSection from '@/components/hero/HeroSection';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <FeatureSection />
     </main>
   );
 }
