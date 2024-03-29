9b3bfae6c47f8ff52aa86b81bcf4bdef6109a198
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-29 01:35:54 -0400

    Update tailwind.config.js with new screen sizes and font families.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,20 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      letterSpacing: {
-        widest: '.3em',
-      },
-    },
     screens: {
       sm: '480px',
       md: '768px',
       lg: '976px',
       xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+      },
+      letterSpacing: {
+        widest: '.3em',
+      },
     },
   },
   plugins: [],
