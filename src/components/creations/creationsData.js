b7976a76ead702396488ca089b1311879a0b6bd1
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-29 01:39:35 -0400

    Update image paths.

--- src/components/creations/creationsData.js
+++ src/components/creations/creationsData.js
@@ -1,49 +1,52 @@
-export const CREATIONS_CARDS_DATA = [
+export const FIRST_ROW_DATA = [
   {
-    desktopImg: '/loopstudios-web-app/images/desktop/image-deep-earth.jpg',
-    mobileImg: '/loopstudios-web-app/images/mobile/image-deep-earth.jpg',
+    desktopImg: '/images/desktop/image-deep-earth.jpg',
+    mobileImg: '/images/mobile/image-deep-earth.jpg',
     alt: 'Deep Earth',
     title: 'Deep earth',
   },
   {
-    desktopImg: '/loopstudios-web-app/images/desktop/image-night-arcade.jpg',
-    mobileImg: '/loopstudios-web-app/images/mobile/image-night-arcade.jpg',
+    desktopImg: '/images/desktop/image-night-arcade.jpg',
+    mobileImg: '/images/mobile/image-night-arcade.jpg',
     alt: 'Night Arcade',
     title: 'Night arcade',
   },
   {
-    desktopImg: '/loopstudios-web-app/images/desktop/image-soccer-team.jpg',
-    mobileImg: '/loopstudios-web-app/images/mobile/image-soccer-team.jpg',
+    desktopImg: '/images/desktop/image-soccer-team.jpg',
+    mobileImg: '/images/mobile/image-soccer-team.jpg',
     alt: 'Soccer Team VR',
     title: 'Soccer team VR',
   },
   {
-    desktopImg: '/loopstudios-web-app/images/desktop/image-grid.jpg',
-    mobileImg: '/loopstudios-web-app/images/mobile/image-grid.jpg',
+    desktopImg: '/images/desktop/image-grid.jpg',
+    mobileImg: '/images/mobile/image-grid.jpg',
     alt: 'The Grid',
     title: 'The grid',
   },
+];
+
+export const SECOND_ROW_DATA = [
   {
-    desktopImg: '/loopstudios-web-app/images/desktop/image-from-above.jpg',
-    mobileImg: '/loopstudios-web-app/images/mobile/image-from-above.jpg',
+    desktopImg: '/images/desktop/image-from-above.jpg',
+    mobileImg: '/images/mobile/image-from-above.jpg',
     alt: 'From Up Above VR',
     title: 'From up above VR',
   },
   {
-    desktopImg: '/loopstudios-web-app/images/desktop/image-pocket-borealis.jpg',
-    mobileImg: '/loopstudios-web-app/images/mobile/image-pocket-borealis.jpg',
+    desktopImg: '/images/desktop/image-pocket-borealis.jpg',
+    mobileImg: '/images/mobile/image-pocket-borealis.jpg',
     alt: 'Pocket Borealis',
     title: 'Pocket borealis',
   },
   {
-    desktopImg: '/loopstudios-web-app/images/desktop/image-curiosity.jpg',
-    mobileImg: '/loopstudios-web-app/images/mobile/image-curiosity.jpg',
+    desktopImg: '/images/desktop/image-curiosity.jpg',
+    mobileImg: '/images/mobile/image-curiosity.jpg',
     alt: 'The Curiosity',
     title: 'The curiosity',
   },
   {
-    desktopImg: '/loopstudios-web-app/images/desktop/image-fisheye.jpg',
-    mobileImg: '/loopstudios-web-app/images/mobile/image-fisheye.jpg',
+    desktopImg: '/images/desktop/image-fisheye.jpg',
+    mobileImg: '/images/mobile/image-fisheye.jpg',
     alt: 'Make It Fisheye',
     title: 'Make it fisheye',
   },
