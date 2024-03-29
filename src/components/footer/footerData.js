7d75e506e3b446d8fbe4a2fc30700fc3ddc5be20
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-29 01:40:21 -0400

    Add footer data and icons.

---
+++ src/components/footer/footerData.js
@@ -0,0 +1,14 @@
+export const MENU_ITEMS_DATA = [
+  'About',
+  'Careers',
+  'Events',
+  'Products',
+  'Support',
+];
+
+export const SOCIAL_ICONS_DATA = [
+  { src: '/images/icon-facebook.svg', alt: 'Facebook' },
+  { src: '/images/icon-twitter.svg', alt: 'Twitter' },
+  { src: '/images/icon-pinterest.svg', alt: 'Pinterest' },
+  { src: '/images/icon-instagram.svg', alt: 'Instagram' },
+];
