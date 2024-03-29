ce07127cac00f9c3e498618c59ea7e3495befe95
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-29 00:35:53 -0400

    Add creationsData.js file with initial data.

---
+++ src/components/creations/creationsData.js
@@ -0,0 +1,50 @@
+export const CREATIONS_CARDS_DATA = [
+  {
+    desktopImg: '/loopstudios-web-app/images/desktop/image-deep-earth.jpg',
+    mobileImg: '/loopstudios-web-app/images/mobile/image-deep-earth.jpg',
+    alt: 'Deep Earth',
+    title: 'Deep earth',
+  },
+  {
+    desktopImg: '/loopstudios-web-app/images/desktop/image-night-arcade.jpg',
+    mobileImg: '/loopstudios-web-app/images/mobile/image-night-arcade.jpg',
+    alt: 'Night Arcade',
+    title: 'Night arcade',
+  },
+  {
+    desktopImg: '/loopstudios-web-app/images/desktop/image-soccer-team.jpg',
+    mobileImg: '/loopstudios-web-app/images/mobile/image-soccer-team.jpg',
+    alt: 'Soccer Team VR',
+    title: 'Soccer team VR',
+  },
+  {
+    desktopImg: '/loopstudios-web-app/images/desktop/image-grid.jpg',
+    mobileImg: '/loopstudios-web-app/images/mobile/image-grid.jpg',
+    alt: 'The Grid',
+    title: 'The grid',
+  },
+  {
+    desktopImg: '/loopstudios-web-app/images/desktop/image-from-above.jpg',
+    mobileImg: '/loopstudios-web-app/images/mobile/image-from-above.jpg',
+    alt: 'From Up Above VR',
+    title: 'From up above VR',
+  },
+  {
+    desktopImg: '/loopstudios-web-app/images/desktop/image-pocket-borealis.jpg',
+    mobileImg: '/loopstudios-web-app/images/mobile/image-pocket-borealis.jpg',
+    alt: 'Pocket Borealis',
+    title: 'Pocket borealis',
+  },
+  {
+    desktopImg: '/loopstudios-web-app/images/desktop/image-curiosity.jpg',
+    mobileImg: '/loopstudios-web-app/images/mobile/image-curiosity.jpg',
+    alt: 'The Curiosity',
+    title: 'The curiosity',
+  },
+  {
+    desktopImg: '/loopstudios-web-app/images/desktop/image-fisheye.jpg',
+    mobileImg: '/loopstudios-web-app/images/mobile/image-fisheye.jpg',
+    alt: 'Make It Fisheye',
+    title: 'Make it fisheye',
+  },
+];
