41ac5128dde00e38e558a8d5153d932abd66886b
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-29 01:38:20 -0400

    Update creations section with new data.

--- src/components/creations/CreationsSection.jsx
+++ src/components/creations/CreationsSection.jsx
@@ -1,5 +1,5 @@
 import CreationsItem from './CreationsItem';
-import { CREATIONS_CARDS_DATA } from './creationsData';
+import { FIRST_ROW_DATA, SECOND_ROW_DATA } from './creationsData';
 const CreationsSection = () => {
   return (
     <section id='creations'>
@@ -8,11 +8,22 @@ const CreationsSection = () => {
           <h2 className='text-4xl text-center uppercase md:text-left md:text-5xl'>
             Our creations
           </h2>
-          <button className='hidden btn-loopstudios md:block'>See all</button>
+          <button className='hidden btn md:block'>See all</button>
         </div>
 
-        <div className='item-container-loopstudios'>
-          {CREATIONS_CARDS_DATA.map((item, index) => (
+        <div className='item-container'>
+          {FIRST_ROW_DATA.map((item, index) => (
+            <CreationsItem
+              key={index}
+              desktopImg={item.desktopImg}
+              mobileImg={item.mobileImg}
+              alt={item.alt}
+              title={item.title}
+            />
+          ))}
+        </div>
+        <div className='item-container mt-10'>
+          {SECOND_ROW_DATA.map((item, index) => (
             <CreationsItem
               key={index}
               desktopImg={item.desktopImg}
@@ -24,7 +35,7 @@ const CreationsSection = () => {
         </div>
 
         <div className='flex justify-center mt-10 md:hidden'>
-          <button className='btn-loopstudios w-full md:hidden'>See all</button>
+          <button className='btn w-full md:hidden'>See all</button>
         </div>
       </div>
     </section>
